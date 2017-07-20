928f407535715c5f77bbae2a188aaf64a41284ec
Alessandro Nocella <alessandro.nocella@phone4biz.it>
2017-07-20 16:30:19 +0200

    sistemata animazione tasto aggiorna meteo

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,17 +1,15 @@
 // sole che cambia rotazione
-$("#logo").hover(
-  function() {
-	$(this).removeClass().addClass("counter-spin");
-  }, function() {
-	$(this).removeClass().addClass("spin");
-  }
-);
+$("#logo").mouseenter(function() {
+   $(this).removeClass().addClass("counter-spin");
+});
+$("#logo").mouseleave(function() {
+	 $(this).removeClass().addClass("spin");
+});
 
 // rotazione tasto refresh
-$("#refresh_weather").hover(
-  function() {
-	$("#refresh_weather i").addClass("fa-spin");
-  }, function() {
-	$("#refresh_weather i").removeClass("fa-spin");
-  }
-);
+$("body").on("mouseenter","#refresh_weather", function() {
+   $("#refresh_weather i").addClass("fa-spin");
+});
+$("body").on("mouseleave","#refresh_weather", function() {
+	 $("#refresh_weather i").removeClass("fa-spin");
+});
